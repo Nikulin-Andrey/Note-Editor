8acb995fa561d4feea7b8bce6a751d721d418a85
Nikulin Andrey <andrey_nikulin3@mail.ru>
2021-11-25 18:55:45 +0300

    add NoteHeader component and comments

--- src/components/notesArea/Note.js
+++ src/components/notesArea/Note.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import Button from '../Button/Button';
 import NoteContent from './NoteContent';
+import NoteHeader from './NoteHeader';
 
 export default function Note(props) {
 
     return (
         <div className="note">
-            <div className="note_header">
-                <Button
-                    id={props.id}
-                    onClick={props.editNote}
-                    className="note_edit"
-                    value={props.edit ? "Save" : "Edit"}
-                />
-                <Button
-                    id={props.id}
-                    onClick={props.deleteNote}
-                    className="note_delete"
-                    value="Delete"
-                />
-            </div>
+            <NoteHeader
+                id={props.id}
+                editNote={props.editNote}
+                deleteNote={props.deleteNote}
+                edit={props.edit}
+            />
             <NoteContent
                 isEdit={props.edit}
                 text={props.text}
